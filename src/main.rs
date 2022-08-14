3f07b50c6541cddbbc35ccce676e2e90d8fc51d5
Javid izadi <javid.izadi1384@gmail.com>
2022-08-14 09:36:12 +0430

    init commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+fn main() {
+    print!("Enter your weight in KG : ");
+    io::stdout().flush().unwrap();
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).unwrap();
+    let weight: f32 = user_input.trim().parse().unwrap();
+    let mars_weight = calculate_on_mars(weight);
+    println!("Weight on Mars = {}Kg", mars_weight);
+}
+fn calculate_on_mars(weight: f32) -> f32 {
+    weight / 9.81 * 3.711
+}
